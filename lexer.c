1af478c1953b63945dddd05aa9d0c481319252ce
zlx <zlx20010815@gmail.com>
2026-05-10 13:08:12 +0100

    modified:   lexer.c

--- lexer.c
+++ lexer.c
@@ -355,7 +355,7 @@ static Literal literal(TokenType t, char *lexeme)
 							c += lexeme[i] - '0';
 							i++;
 						}
-						if (c <= UINT8_MAX)
+						if (c <= INT8_MAX)
 							r.string[j++] = (char)c;
 						else
 							r.string[j++] = lexeme[i];
