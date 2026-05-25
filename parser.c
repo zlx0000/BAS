cc1ca0620c649abf217aaab851d1008edb4503b8
zlx <zlx20010815@gmail.com>
2026-05-25 05:36:53 +0100

    modified:   parser.c

--- parser.c
+++ parser.c
@@ -517,6 +517,8 @@ ParseTreeNode *parseRelExpr(ParserContext *context)
 		struct ParseTreeNode *node3 = parseAddExpr(context);
 		ERR_RET(node3);
 		node->children[cnt++] = node3;
+	} else {
+		ERR("Expected relop");
 	}
 	node->childCount = cnt;
 	return node;
