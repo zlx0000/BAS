05990ed9fc6a75cf99967e441c16d073c8e518d2
zlx <zlx20010815@gmail.com>
2026-08-08 17:53:17 +0100

    modified:   gbasic.c

--- gbasic.c
+++ gbasic.c
@@ -15,7 +15,7 @@ int pc;
 int expectedLineNum;
 Stack shadow_st;
 
-#ifndef __WINDOWS__
+#ifndef WIN32
 static char *keyword_generator(const char *text, int state)
 {
     static int index;
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
 	prog.lineCount = 0;
 	prog.lines = (ParseTreeNode **)calloc(16384, sizeof (ParseTreeNode *));
 	prog.shadow_st = (Stack *)calloc(16384, sizeof (Stack));
-#ifndef __WINDOWS__
+#ifndef WIN32
 	rl_variable_bind("enable-bracketed-paste", "off");
 	rl_attempted_completion_function = gbasic_completion;
 	using_history();
@@ -91,7 +91,7 @@ repl:
 		.type = INT_VAL,
 	};
 	if (isatty(STDIN_FILENO)) {
-#ifdef __WINDOWS__
+#ifdef WIN32
 		printf(">");
 		str = calloc(STR_SIZE, sizeof(char));
 		if (!fgets(str, STR_SIZE, stdin)) {
